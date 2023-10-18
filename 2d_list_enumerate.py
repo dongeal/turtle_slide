5eaa9a0fb2c1c4d11936bd09327a7cd3c5bd33ca
dongeal <dongeal@gmail.com>
2023-10-18 13:38:03 +0900

    연습 파일

--- 2d_list_enumerate.py
+++ 2d_list_enumerate.py
@@ -4,3 +4,16 @@ for i ,factor in enumerate(list):
     print(i, factor)
     if t in factor:
         print(i, factor.index(t))
+
+import turtle
+
+screen = turtle.Screen()
+
+key = turtle.Turtle()
+key.shapesize(5)
+
+screen.onclick(lambda a,b :key.goto(a,b))
+
+
+
+turtle.mainloop()
